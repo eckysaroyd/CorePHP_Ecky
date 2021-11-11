18e498db3d1139a9fa020bb84b8a7d0cb46461a8
eckysaroyd <enyato98@gmail.com>
2021-11-11 18:26:08 +0530

    new added webpage

---
+++ day1_Assignments/footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Mimosa</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- *******************--------------footer startedd=----------------********************  -->
+<footer>
+  <div class="container-fluid top-space4">
+    <div class="row">
+      <div class="col-md-4 downFooterColors  text-center">
+        <h3 class="downFooterColors"><strong class="downFooterFont"> Mimosa</strong><br>Consolting</h3>
+        <p>
+          <!-- facebook icon -->
+         <a href="#" style="text-decoration: none; color: #ffffff;">
+          <i class="fab fa-facebook" aria-hidden="true"></i>
+        </a>
+        <!-- instagram icon -->
+        <a href="#" style="text-decoration: none; color: #ffffff;">
+        <i class="fab fa-instagram" aria-hidden="true"></i>
+        </a>
+       <!--  twitter font awesome link -->
+       <a href="#" style="text-decoration: none; color: #ffffff;">
+       <i class="fab fa-twitter"></i>
+     </a>
+        </p>
+
+      </div>
+      <div class="col-md-4 downFooterColors text-center downFooterColors">
+        <h4>About</h4>
+        <p><a href="#" class="a1">About</a><br>
+            <a href="#" class="a1">Contact</a>
+        </p>
+      </div>
+      <div class="col-md-4 downFooterColors text-center downFooterColors">
+        <h4>Service</h4>
+        <p><a href="#" class="a1">Story band</a><br>
+            <a href="#" class="a1">Clalify your Message</a>
+        </p>
+      </div>
+  </div>
+  <div class="container-fluid downFooterColors text-center downFooterColors">
+    <h1>&nbsp;&nbsp;&nbsp;<br><br></h1>
+    <h4 class="downFooterColors" style="font-size: 12px;">&copy;2019 Your name. All right reserved. Tempalate Design by <a href="#" class="a1">Protolabz</a></h4>
+  </div>
+</div>
+</footer>
+<!-- Footer -->
+</body>
+</html>
---
+++ day1_Assignments/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Mimosa</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light container-fluid header1">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand  font-weight-bold" href="#"><span style="font-family:'Dancing Script', cursive; color: #ffffff;">Mimosa</span><br><span style="color: #ffffff;"> Consulting</span></a>
+
+  <div class="collapse navbar-collapse header1" id="navbarTogglerDemo03">
+    <div  class="navbar-nav mr-auto mt-2 mt-lg-0">
+    </div>
+    <div class="form-inline my-2 my-lg-0">
+    
+     <ul class="navbar-nav mr-auto mt-2 mt-lg-0 font-weight-bold" style="color: #ffffff;">
+      <li class="nav-item header1">
+        <a class="nav-link" href="#" style="color: #ffffff;">ABOUT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #ffffff;">SERVICE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #ffffff;">PORTFOLIO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #ffffff;">BLOG</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 header1">
+  
+        <button type="button" class="pl-4 pr-4 pt-2 pb-2 btn btn-default font-weight-bold" style= "background-color:#f7f6f2; border-radius: 30px; color: #2a7f85; border:0px;">SCHEDULE A CALL</button>  
+    </form>
+  </div>
+</nav>
+</body>
+</html>
