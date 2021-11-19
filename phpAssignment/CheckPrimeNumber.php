0cbaa5afc35d38dec00c2527294f8f6b71f06d1e
eckysaroyd <enyato98@gmail.com>
2021-11-19 18:32:32 +0530

    new added file

---
+++ phpAssignment/CheckPrimeNumber.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>NEW ADDED FORM</title>
+</head>
+<body>
+</body>
+</html>
+<?php
+
+$numbers = 100;
+
+for($i=2; $i<=$numbers; $i++)
+{
+  $isPrime=true;
+  for($j=2; $j<=$j++)
+
+  {
+   if($isPrime == true)
+  {
+    echo $i." ";
+  } 
+}
+
+?>
