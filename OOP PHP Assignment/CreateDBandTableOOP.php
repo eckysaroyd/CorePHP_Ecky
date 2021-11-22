6cbdc585e72840e36fe18032a7dd8feed57e7e36
eckysaroyd <enyato98@gmail.com>
2021-11-22 16:46:11 +0530

    new OOP PROJECT

--- OOP PHP Assignment/CreateDBandTableOOP.php
+++ OOP PHP Assignment/CreateDBandTableOOP.php
@@ -1,4 +1,25 @@
+<?php
 $serverName = "localhost";
-$UserName =
-$Password =
-$DBname =
+$UserName = "root";
+$Password = "";
+$DBname = "eckyprotolabz";
+
+$conn = new mysqli($serverName, $UserName, $Password, $DBname);
+if($conn->connect_error)
+	{
+		 die ("Conneciton Failed.!!".$conn->connect_error);
+	}
+
+//begginning of New SQL Table
+	$sql = "CREATE TABLE newoop(id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,fname VARCHAR(20) NOT NULL, lname VARCHAR(20) NOT NULL, email VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+	if($conn->query($sql)=== TRUE)
+	{
+		echo "TABLE WAS CREATED SUCCESSFULLY!";
+		
+	}
+	else
+		{
+			echo "WE HAVE ERROR IN CREATING TABLE". $conn->error;
+		}
+$conn->close();
+?>
