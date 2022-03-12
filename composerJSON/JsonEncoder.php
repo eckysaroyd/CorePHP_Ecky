4a9b6b559191f25f20d28d73e4aa1ce668bb869e
eckysaroyd <enyato98@gmail.com>
2022-03-12 08:38:00 +0530

    new added core php projercts

--- composerJSON/JsonEncoder.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$age = array('john'=>70,'ALI'=>60,'ABBU'=>80);
-	$cars = array("Volvo", "BMW", "Toyota");
-	echo json_encode($age);
-	echo "<br>";
-	echo json_encode($cars);
-	echo "<br>";
-?>
